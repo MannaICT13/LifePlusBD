877e27c814246c1e6e5ff79bf3bf7f6490561745
MannaICT13 <khmannaict13@gmail.com>
2021-03-17 21:46:08 +0600

    created DetailVC and design UI and create there outlets

---
+++ LifePlusBD/Controller/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  LifePlusBD
+//
+//  Created by Md Khaled Hasan Manna on 17/3/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    //MARK:- Properties
+    
+    @IBOutlet weak var idLbl: UILabel!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var language: UILabel!
+    @IBOutlet weak var statusLbl: UILabel!
+    @IBOutlet weak var runtimeLbl: UILabel!
+    
+    //MARK:- Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+
+}
